70240a8dce707b97ada34181ea45eb69cb809d10
Manu <manuel.orc1@gmail.com>
2023-10-27 10:08:16 +0200

    Holi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.helloworld"
     compileSdk = 33
 
+    viewBinding{
+        enable=true
+    }
+
     defaultConfig {
         applicationId = "com.example.helloworld"
         minSdk = 24
